e3346e48eaef61338e18dc003b5eb12324474718
sami jaadar <56087072+samijaadar@users.noreply.github.com>
2024-10-27 21:18:38 +0100

    Update index.py

--- index.py
+++ index.py
@@ -3,6 +3,6 @@ import streamlit as st
 pg = st.navigation([
         st.Page("main.py", title="External Factor Data"),
         st.Page("page_2.py", title="LangChain"),
-        st.Page("page_2.py", title="Supplier Data"),
+        st.Page("page_3.py", title="Supplier Data"),
     ])
 pg.run()
